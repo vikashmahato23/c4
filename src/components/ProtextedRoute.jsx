d9524d874168ac67d38c4379b660d2420af25e5a
Vikash Mahato <94161952+vikashmahato23@users.noreply.github.com>
2022-05-06 18:58:45 +0530

    Update ProtextedRoute.jsx

--- src/components/ProtextedRoute.jsx
+++ src/components/ProtextedRoute.jsx
@@ -1,11 +1,11 @@
 import { useSelector } from "react-redux";
 import { store } from "../Redux/store";
 
-// /orders and /neworder are protected routes
+
 export const ProtectedRoute = ({ children }) => {
   const isAuth = useSelector((store) => store.isLoggedIn);
 
-  //   if (isAuth)
+
   return children;
-  //   else return;
+
 };
